c638aa15f6e1d4f9b0426cf7ebab022df445374d
Erik Nilsen <enilsen16@live.com>
2014-10-29 13:36:17 -0700

    improve solution

--- greedy.c
+++ greedy.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
+void change (int d);
+
 int main(void)
 {
   printf("How much change is owed?\n");
@@ -11,14 +13,70 @@ int main(void)
     n = GetFloat();
   };
   printf("You want me to give you this much change?\n %.2f \n", n);
-  int d = roundf(n * 100);
-  int q = d/25;
-  int e = d%25;
-  int dime = e/10;
-  int h = e%10;
-  int nickel = h/5;
-  int i = h%5;
-  int penny = i/1;
-  printf("Your change is %d quarters, %d dimes, %d nickels, and %d pennies", q, dime, nickel, penny);
-
-}
+  int d = roundf(n*100);
+  printf ("Your change is ");
+  change(d);
+};
+  void change(int d) {
+    if (d >= 25)
+    {
+      int quarter = d/25;
+      if ( quarter > 1)
+      {
+        printf("%d quarters ", quarter);
+      }
+      else
+      {
+        printf("%d quarter ", quarter);
+      }
+      int e = d%25;
+      d = e;
+      change(d);
+    }
+    else if (d >= 10)
+    {
+      int dime = d/10;
+      if ( dime > 1)
+        {
+          printf("%d dimes ", dime);
+        }
+      else
+        {
+          printf("%d dime ", dime);
+        }
+        int h = d%10;
+        d = h;
+        change(d);
+    }
+    else if (d >= 5)
+    {
+      int nickel = d/5;
+      if ( nickel > 1 )
+        {
+          printf("%d nickels ", nickel);
+        }
+      else
+        {
+          printf("%d nickel ", nickel);
+        }
+        int i = d%5;
+        d = i;
+        change(d);
+    }
+    else if ( d >= 1)
+      {
+        int penny = d/1;
+        if (penny > 1)
+          {
+            printf("%d pennies", penny);
+          }
+        else
+          {
+            printf("%d penny", penny);
+          }
+      }
+    else
+      {
+        printf("\n");
+      }
+  };
